65e67c3aa3991270b4b7c3b4600567736e07e549
hasanuysaal <hsnuysaal@icloud.com>
2022-08-31 21:26:54 +0300

    Initial Commit

---
+++ ShoopingNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoopingNotes
+//
+//  Created by Hasan Uysal on 31.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any sadditional setup after loading the view.
+    }
+
+
+}
+
