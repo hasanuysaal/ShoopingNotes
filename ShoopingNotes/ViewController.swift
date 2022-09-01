4de1d2736146e0caed8fd881a8ff68fedd891ae4
hasanuysaal <hsnuysaal@icloud.com>
2022-09-01 15:26:58 +0300

    show datas from CoreData on tableview

--- ShoopingNotes/ViewController.swift
+++ ShoopingNotes/ViewController.swift
@@ -6,21 +6,73 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var nameArr = [String]()
+    var idArr = [UUID]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addBtn))
+        
+        getDatas()
         
     }
 
     @objc func addBtn(){
-        
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArr[indexPath.row]
+        return cell
+    }
+    
+    func getDatas() {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "ShopNote")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try context.fetch(fetchRequest)
+            
+            for result in results as! [NSManagedObject] {
+                
+                if let name = result.value(forKey: "name") as? String {
+                    nameArr.append(name)
+                }
+                
+                if let id = result.value(forKey: "id") as? UUID {
+                    idArr.append(id)
+                }
+                
+            }
+            
+            tableView.reloadData()
+            
+        } catch {
+            
+            print("hata")
+        
+        }
         
     }
     
